0fb605d1e77629086fb09439c1a24bc5bcb1e18b
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 <Vivekpatil254@gmail.com>
2022-04-10 15:08:08 +0530

    UC1 and UC2 First name and last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,4 +1,58 @@
+import java.util.regex.*;
+import java.util.Scanner;
+import java.lang.*;
 
-public class UserRegistration {
+public class UserRegistration
+{
 
+	public static void main(String args[])
+	{
+		String regex = "(^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*)";
+		String firstName,lastName;
+		Scanner scanner=new Scanner(System.in);
+		
+		System.out.println("Enter the first name of a user, it should start with capital letter and minimum three charecter long");
+		firstName=scanner.nextLine();
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(firstName);
+		boolean b=matcher.matches();
+		if(b==false)
+		{		System.out.println("Enter name in proper formate, Example 'Abc, Xyz,etc'");
+				firstName=scanner.nextLine();
+				 pattern = Pattern.compile(regex);
+				 matcher=pattern.matcher(firstName);
+				 b=matcher.matches();
+											
+					if(b==false)
+					{
+						System.out.println("You entered a name wrong again");
+						System.exit(0);
+					}	
+		}
+		
+		System.out.println(firstName);
+		
+		System.out.println("Enter the last name of a user, it should start with capital letter and minimum three charecter long");
+		lastName=scanner.nextLine();
+		pattern = Pattern.compile(regex);
+		matcher=pattern.matcher(lastName);
+		b=matcher.matches();
+		if(b==false)
+		{		System.out.println("Enter name in proper formate, Example 'Abc, Xyz,etc'");
+				firstName=scanner.nextLine();
+				 pattern = Pattern.compile(regex);
+				 matcher=pattern.matcher(lastName);
+				 b=matcher.matches();
+											
+					if(b==false)
+					{
+						System.out.println("You entered a name wrong again");
+						System.exit(0);
+					}	
+		}
+		
+		System.out.println(lastName);
+		
+	}
+	
 }
